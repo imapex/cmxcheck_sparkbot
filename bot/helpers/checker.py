cb4dfd042078f0a8d038e9cd06fb643e843bf0c6
josuhr <josuhr@cisco.com>
2017-05-10 20:09:56 -0500

    restructure bot folder

---
+++ bot/helpers/checker.py
@@ -0,0 +1,24 @@
+# ToDo
+#   Format a plain hostname into a url
+#   Implement proper HTTP code handling to reflect redirects etc.
+#   Error handling with urllib2
+#   Add authenticated API calls to query CMX statss
+
+import httplib
+
+def get_status_code(host, path="/"):
+    """ This function retreives the status code of a website by requesting
+        HEAD data from the host. This means that it only requests the headers.
+        If the host cannot be reached or something else goes wrong, it returns
+        None instead.
+    """
+    try:
+        conn = httplib.HTTPConnection(host)
+        conn.request("HEAD", path)
+        return conn.getresponse().status
+    except StandardError as e:
+        return e
+
+
+print get_status_code("stackoverflow.com") # prints 200
+print get_status_code("stackoverflow.com", "/nonexistant") # prints 404
